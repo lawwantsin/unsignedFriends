a9bf95251607ea16e67fb426d772ae35e29440ed
Lawrence <law@cinemasetfree.com>
2014-11-14 16:49:03 -0800

    working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  post '/' => 'home#index'
   post 'match_to_list' => 'home#match'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
